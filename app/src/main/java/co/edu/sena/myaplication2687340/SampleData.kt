d90e4f4551a521b4233dc79307e401b16843baa1
Your Name <you@example.com>
2023-08-11 13:21:11 -0500

    Android studio project

---
+++ app/src/main/java/co/edu/sena/myaplication2687340/SampleData.kt
@@ -0,0 +1,67 @@
+package co.edu.sena.myaplication2687340
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Jhoan",
+            "Hola bro ¿Vamo al futibol?"
+        ),
+        Message(
+            "Mario",
+            "Claro, pero en cual cancha : ..más\n" +
+                    "La piloto\n" +
+                    "Al frente de la casa de ronald\n" +
+                    "La que queda por mi casa\n"
+        ),
+        Message(
+            "Jaykel",
+            "Pues no se mano\n"
+        ),
+        Message(
+            "Jhoan",
+            "Yo digo piloto"
+        ),
+        Message(
+            "Ronald",
+            "podemos ir     ...más\n" +
+                    "La serena\n"
+        ),
+        Message(
+            "Jhoan",
+            "Pues la verdad no tengo idea"
+        ),
+        Message(
+            "Jorge",
+            "Que"
+        ),
+        Message(
+            "Jhoan",
+            "so"
+        ),
+        Message(
+            "Ronald",
+            ">:v"
+        ),
+        Message(
+            "Jaykel",
+            "Pero yo no tengo plata"
+        ),
+        Message(
+            "Mario",
+            "Y quien le esta pidiendo"
+        ),
+        Message(
+            "Jhoan",
+            "Yo llevo el balón"
+        ),
+        Message(
+            "Ronald",
+            "listo"
+        ),
+        Message(
+            "George",
+            "listo"
+        ),
+    )
+}
